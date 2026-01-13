fb845b7e0e75224e7e8726957e49b86a37e5eefa
amyv14 <144635611+amyv14@users.noreply.github.com>
2026-01-12 19:50:55 -0500

    updated constants
    Replaced hardcoded database connection parameters with constants.

--- bowow/Bowow-Builder/db/read.py
+++ bowow/Bowow-Builder/db/read.py
@@ -4,11 +4,11 @@ from constants import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
  
  # Connect to local PostgreSQL
 conn = psycopg.connect(
-    dbname="itemsdb",
-    user="estellegerber",
-    password="megu$taDatab$$3s",
-    host="bowwow-db.cneo2g2w2qei.us-east-2.rds.amazonaws.com",
-    port="5432"
+    dbname="DB_NAME",
+    user="DB_USER",
+    password="DB_PASSWORD",
+    host="DB_HOST",
+    port="DB_PORT"
 
 )
 cur = conn.cursor()
