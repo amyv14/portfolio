a04b1d6273fa7cff1814003436563ea1210cb419
amyv14 <144635611+amyv14@users.noreply.github.com>
2026-01-12 19:52:09 -0500

    Update database connection constants

--- bowow/Bowow-Builder/db/constants.py
+++ bowow/Bowow-Builder/db/constants.py
@@ -1,8 +1,6 @@
-DB_NAME = "itemsdb"
-DB_USER = "estellegerber"
-DB_PASSWORD = ""
-DB_HOST = "localhost"
-DB_PORT = "5432"
+DB_NAME = "super"
+DB_USER = "super"
+DB_PASSWORD = "secretive"
+DB_HOST = "and"
+DB_PORT = "private"
 INVALID_SALT = 2
-
-# host="bowwow-db.cneo2g2w2qei.us-east -2.rds.amazonaws.com"
